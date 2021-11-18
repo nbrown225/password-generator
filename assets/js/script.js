1eda05bb6201c75b74f0c2d43b34130f1f90e542
nbrown225 <chbrown225@yahoo.com>
2021-11-18 07:15:12 -0800

    created a form, gave inputs for each part of the form, styled the form background, text and allignment. re-added psudocode for notes sake.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
+// array for alphabet
+// array for 0-9
+// array for special characters 
 
+const lowerCaseAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const upperCaseAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const symbols = ["@","!", "#", "$", "%", "^", "&", "*"];
+
+// prompt would you like to create a password //
+
+var generate = confirm("Are you ready?");
+if(generate) {
+    prompt(<select>lowerCaseAlpha</select>)
+}
+   
+// how many charactors would you like it to be 8-128
+    // prompt to let them choose the number
+// prompt would you like to include numbers?
+    // yes/no
+// prompt would you like special characters?
+    // yes/no
+// password has been created
+    // prompt to copy password
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
